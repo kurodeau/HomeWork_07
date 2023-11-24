502c77132b1116e7ee049f8e7c88b5e889a9da5d
Kuro.Wu <kurome123@gmail.com>
2023-11-24 18:37:33 +0800

    new hw4

--- src/hw1/HW1.java
+++ src/hw1/HW1.java
@@ -3,31 +3,31 @@ package hw1;
 public class HW1 {
 
 	public static void main(String[] args) {
-		// 題目一
-		int num1 = 12;
-		int num2 = 6;
-		int product = num1 * num2; // 積
-		System.out.println(num1 + "和" + num2 + "的乘積是" + product);
-		int sum = num1 + num2; // 和
-		System.out.println(num1 + "和" + num2 + "的加總是" + sum);
-
-		// 題目二
-		int egg_nums = 200;
-		final int DOZEN = 12;
-
-		int dozen_nums = egg_nums / DOZEN;
-		System.out.println("一共是" + dozen_nums + "打");
-		int eggs_scattered = egg_nums % DOZEN;
-		System.out.println("而且剩下" + eggs_scattered + "顆");
-		System.out.println("一共是" + dozen_nums + "打");
-
-		// 題目三
-		int time_sec = 256559;
-		int sec_nums = time_sec % 60; // 計算分鐘之外的剩餘秒數
-		int min_nums = time_sec % (60 * 60 * 24) / (60 * 60) / (60); // 計算小時之外的剩餘分鐘數
-		int hour_nums = time_sec % (60 * 60 * 24) / (60 * 60); // 計算日之外的剩餘時數
-		int days_nums = time_sec / (60 * 60 * 24); // 計算小時之外的剩餘秒數
-		System.out.println(time_sec + "是" + days_nums + "天," + hour_nums + "時," + min_nums + "分," + sec_nums + "秒");
+//		// 題目一
+//		int num1 = 12;
+//		int num2 = 6;
+//		int product = num1 * num2; // 積
+//		System.out.println(num1 + "和" + num2 + "的乘積是" + product);
+//		int sum = num1 + num2; // 和
+//		System.out.println(num1 + "和" + num2 + "的加總是" + sum);
+//
+//		// 題目二
+//		int egg_nums = 200;
+//		final int DOZEN = 12;
+//
+//		int dozen_nums = egg_nums / DOZEN;
+//		System.out.println("一共是" + dozen_nums + "打");
+//		int eggs_scattered = egg_nums % DOZEN;
+//		System.out.println("而且剩下" + eggs_scattered + "顆");
+//		System.out.println("一共是" + dozen_nums + "打");
+//
+//		// 題目三
+//		int time_sec = 256559;
+//		int sec_nums = time_sec % 60; // 計算分鐘之外的剩餘秒數
+//		int min_nums = time_sec % (60 * 60 * 24) / (60 * 60) / (60); // 計算小時之外的剩餘分鐘數
+//		int hour_nums = time_sec % (60 * 60 * 24) / (60 * 60); // 計算日之外的剩餘時數
+//		int days_nums = time_sec / (60 * 60 * 24); // 計算小時之外的剩餘秒數
+//		System.out.println(time_sec + "是" + days_nums + "天," + hour_nums + "時," + min_nums + "分," + sec_nums + "秒");
 
 		// 題目四
 		final double PI = 3.1415;
@@ -36,27 +36,26 @@ public class HW1 {
 		double perimeter = 2 * r * PI;
 		System.out.print("半徑" + r + "的話:");
 		System.out.printf("面積是%.2f", area);
-		System.out.printf(",圓周是%.2f %n", perimeter);
-
-		// 題目五
-		int money = 1_500_000;
-		double set = 0.05;
-		double money_10year = 1_500_000, years = 10;
-		while (years == 0) {
-			money_10year = money_10year * (1 + set);
-			years = years - 1;
-		}
-		System.out.print(years + "十年後");
-		System.out.printf("存款連本帶利共:%.2f %n", money_10year);
-
-		// 題目六
-		System.out.println(5 +5); //同為int整數型別,彼此正常計算
-		System.out.println(5 +'5'); //'5'被轉成萬國碼所代表的數字()進行計算,可以參考下面註解掉的內容
-//		int sample1 = '5';
-//		System.out.println(sample1);
-		System.out.println(5 +"5"); //"5"是字串,運算內容包含字串,則一律由算術改寫為串接,可以參考下面註解的內容
-//		String sample2 = "5";
-//		System.out.println(sample2 + 7 +sample2);		
+	    System.out.printf(",圓周是%.2f %n", perimeter);
+//		// 題目五
+//		int money = 1_500_000;
+//		double set = 0.05;
+//		double money_10year = 1_500_000, years = 10;
+//		while (years == 0) {
+//			money_10year = money_10year * (1 + set);
+//			years = years - 1;
+//		}
+//		System.out.print(years + "十年後");
+//		System.out.printf("存款連本帶利共:%.2f %n", money_10year);
+//
+//		// 題目六
+//		System.out.println(5 +5); //同為int整數型別,彼此正常計算
+//		System.out.println(5 +'5'); //'5'被轉成萬國碼所代表的數字()進行計算,可以參考下面註解掉的內容
+////		int sample1 = '5';
+////		System.out.println(sample1);
+//		System.out.println(5 +"5"); //"5"是字串,運算內容包含字串,則一律由算術改寫為串接,可以參考下面註解的內容
+////		String sample2 = "5";
+////		System.out.println(sample2 + 7 +sample2);		
 
 	}
 
--- src/hw4/HW4_1.java
+++ src/hw4/HW4_1.java
@@ -4,9 +4,35 @@ public class HW4_1 {
 	
 	public static void main(String[] args) {
 		
-		int[] array1 = {29, 100, 39, 41, 50, 66, 8, 77, 95, 15};
-		HW4_2.findArrayArgMax(array1);
+		//調用第一題_計算陣列的平均數與最大樹
+//		int[] array1 = {29, 100, 39, 41, 50, 66, 8, 77, 95, 15};
+//		HW4_2.findArrayArgMax(array1);
 		
+		//調用第二題_使輸入的字串反過來顯示
+//		String S = ("String");
+//		HW4_2.getStringOpp(S);
+
+		//調用第三題_計算字串array中指定的母音數量
+//		String[] eightPlanets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
+//		HW4_2.countSolarSystem(eightPlanets);
+		
+		//調用第四題_滿足借錢條件, 則印出符合編號與總數
+//		int[][] array4 = { {25, 32, 8, 19, 27}, {2500, 800, 500, 1000, 1200} };
+//		HW4_2.getMoneyFromMath(array);
+		
+//		//調用第五題_輸入年分, 月份, 日期, 計算該日期日該年的第幾天
+//		HW4_2.countDataInYear();
+		
+		//調用第六題_考最高分數的次數
+		int[][] array6 = { 
+				{10, 35, 40, 100, 90, 85, 75 ,70},
+				{37, 75, 77, 89, 64, 75, 70, 95},
+				{100, 70, 79, 90, 75, 70, 79, 90},
+				{77, 95, 70, 89, 60, 75, 85, 89},
+				{98, 70, 89, 90, 75, 90, 89, 90},
+				{90, 80, 100, 75, 50, 20, 99, 75}
+				};
+		HW4_2.countHighestScore(array6);
 	}
 	
 	
