21eabe2dbf9a8168495ce2addd413ee5cfb268a5
Kuro.Wu <kurome123@gmail.com>
2023-11-27 20:17:02 +0800

    new HW5

---
+++ src/hw5/HW5_1.java
@@ -0,0 +1,34 @@
+package hw5;
+
+public class HW5_1 {
+	
+	public static void main(String[] args) {
+		
+		//調用第一題_使用者自行輸入長, 寬並以"*"印出長方形
+//	    HW5_2.starSquare();
+		
+		//調用第二題_使用者自行輸入長, 寬並以"*"印出長方形
+//	    HW5_2.randAvg();
+		
+		//調用第三題_使用Overloading
+		int[][] array1 = {	
+				{1, 6, 3},
+				{9, 5, 2}
+				};
+		double[][] array2 = {	
+				{1.2, 3.5, 2.2},
+				{7.4, 2.1, 8.2}
+				};
+//		HW5_2.maxElement( array2 );
+		
+		//調用第四題_產生8碼隨機包含大小寫英文字母與數字0~9
+	    HW5_2.genAuthCode();
+
+		
+		
+		
+		
+		
+	}
+
+}
