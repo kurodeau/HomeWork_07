3821d9140139a5b92b6ba3f9bcbca7d88143e460
Kuro.Wu <kurome123@gmail.com>
2023-12-11 18:04:15 +0800

    [new] finish hw6

--- src/hw6/Calculator.java
+++ src/hw6/Calculator.java
@@ -1,9 +1,5 @@
 package hw6;
 
-import java.util.Scanner;
-
-import hw6.CalException;
-
 public class Calculator{
 
 	// 宣告類別變數
@@ -13,29 +9,27 @@ public class Calculator{
 	// 宣告建構方法
 	Calculator() {
 	};
-	Calculator(int x, int y) throws CalException {
+	Calculator(int x, int y) throws CalException {		
 		setterxy(x, y);
-	};
+		}
 	// 宣告GS方法
-	public void setterxy(int x, int y) throws CalException {
-		
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("替您計算x 的y 次方");
-		System.out.println("請輸入x");
-		x = scanner.nextInt();
-		System.out.println("請輸入y");
-		y = scanner.nextInt();
-
-		
+	public void setterxy(int x, int y) throws CalException{
 		if ( x == 0 && y == 0 ){
-			throw new CalException("0的 0次方沒有意義...請重新輸入吧");
-			setterxy();
+			throw new CalException("0 的0 次方沒有意義");
+		}
+		else if ( y < 0) {
+			throw new CalException("y請不要是負值");
 		}
 		else {
-			this.x = x;
-			this.y = y;
-			scanner.close();
+			setterx(x);
+			settery(y);
 		}
+	}
+	public void setterx(int x) throws CalException {
+		this.x = x;
+	}
+	public void settery(int y) throws CalException {
+		this.y = y;
 	}
 	public int getterx() {
 		return x;
@@ -44,13 +38,9 @@ public class Calculator{
 		return y;
 	}
 	// 設計powerXY方法為,功能是會計算x的y次方並回傳結果。
-	public int powerXY() {
-		int answer = 1;
-		for (int i = 0; i <= y; i++) {
-			if (i != 0) {
-				answer = answer * x;
-			}
-		}
+	public double powerXY() {
+		double answer;
+		answer = Math.pow(x, y);
 		return answer;
 	}
 }
